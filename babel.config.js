bef35250301f89c431aefeb80cae9d178e74fe77
dutianzhao <dutianzhao@vipkid.com.cn>
2019-03-19 18:52:05 +0800

    init

--- babel.config.js
+++ babel.config.js
@@ -7,14 +7,12 @@ module.exports = {
         'useBuiltIns': 'entry'
       }
     ]
+  ],
+  plugins: [
+    [ 'import', {
+      'libraryName': 'ant-design-vue',
+      'libraryDirectory': 'es',
+      'style': true // `style: true` 会加载 less 文件
+    } ]
   ]
-  // if your use import on Demand, Use this code
-  // ,
-  // plugins: [
-  //   [ 'import', {
-  //     'libraryName': 'ant-design-vue',
-  //     'libraryDirectory': 'es',
-  //     'style': true // `style: true` 会加载 less 文件
-  //   } ]
-  // ]
 }
--- src/core/use.js
+++ src/core/use.js
@@ -3,7 +3,18 @@ import VueStorage from 'vue-ls'
 import config from '@/config/defaultSettings'
 
 // base library
-import Antd from 'ant-design-vue'
+import {
+  Input,
+  Button,
+  Select,
+  Card,
+  Form,
+  Row,
+  Col,
+  Modal,
+  Table,
+  notification
+} from 'ant-design-vue'
 import Viser from 'viser-vue'
 import VueCropper from 'vue-cropper'
 import 'ant-design-vue/dist/antd.less'
@@ -15,7 +26,18 @@ import PermissionHelper from '@/utils/helper/permission'
 
 VueClipboard.config.autoSetContainer = true
 
-Vue.use(Antd)
+Vue.use(Input)
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(Card)
+Vue.use(Form)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Modal)
+Vue.use(Table)
+Vue.use(notification)
+
+Vue.prototype.$notification = notification
 Vue.use(Viser)
 
 Vue.use(VueStorage, config.storageOptions)
